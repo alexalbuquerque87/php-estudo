9da41cc00ce45af267253af9dd955d6ceedda576
Alex Albuquerque <alexalbuquerqueti@gmail.com>
2024-05-12 14:03:35 -0300

    operações matemáticas

---
+++ operacoes.php
@@ -0,0 +1,20 @@
+<?php
+
+$num1 = 2;
+$num2 = 3;
+
+$soma = $num1 + $num2;
+$subtracao = $num1 - $num2;
+$multiplicacao = $num1 * $num2;
+$divisao = $num1 / $num2;
+
+$cubo = $num1 ** $num2;
+
+$resto = $num2 % $num1;
+
+echo "$num1+$num2=$soma\n";
+echo "$num1-$num2=$subtracao\n";
+echo "$num1*$num2=$multiplicacao\n";
+echo "$num1/$num2=$divisao\n";
+echo "$num1^$num2=$cubo\n";
+echo "$num2%$num1=$resto\n";
